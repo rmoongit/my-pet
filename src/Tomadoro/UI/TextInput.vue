be1acc87f1ca3c84a2462b42fd329ed754878a78
rmoongit <emil_tt@mail.ru>
2023-12-27 23:06:50 +0300

    feat: добавляет стили таймеру

---
+++ src/Tomadoro/UI/TextInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <input type="text" :value="value" :disabled="disabled" />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+
+    disabled: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
---
+++ src/components/UI/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1280px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 50px;
+}
+
+@media (max-width: 769px) {
+  .container {
+    padding: 0 20px;
+  }
+}
+</style>
